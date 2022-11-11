712ca098ea535f6786cc47a0ba21d61d4396e1c1
jiayew3 <60640317+jiayew3@users.noreply.github.com>
2022-11-11 03:19:39 -0600

    Add front end
    Add the base google extension front end

---
+++ google-extension/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import React, {useState, useEffect} from "react";
+import { Header, Table} from 'semantic-ui-react'; // header
+import { Input} from 'semantic-ui-react'; // input
+function App() {
+  const [searchInput, setSearchInput] = useState();
+  const [search, setSearch] = useState([]);
+  const SearchPaper = () => {
+  }
+  return (
+    <div className="App">
+    <Header as='h0'>Search for Paper Suggestions for CS410</Header>
+      <div className="form">
+        <Input type="text" placehoder="Search" onChange = {(event) => {
+          setSearchInput(event.target.value);
+        }}/>
+      </div>
+      <button onClick = {SearchPaper}> Search </button>
+    </div>
+  );
+}
+
+export default App;
